dcba82951d86f7a99996d472c95262b30948b84b
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-01-19 01:18:06 -0300

    Criação da função para inicializar e setar os pinos

--- led_putty.c
+++ led_putty.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Definição dos pinos para os LEDs e Buzzer
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define LED_RED 13
+#define BUZZER 21
 
+// Função para inicializar os pinos GPIO
+void init_gpio() {
+    const int leds[] = {LED_RED, LED_GREEN, LED_BLUE};
+    for (int i = 0; i < 3; i++) {
+        gpio_init(leds[i]);
+        gpio_set_dir(leds[i], GPIO_OUT);
+    }
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
+}
 
-int main()
-{
+int main() {
     stdio_init_all();
+    init_gpio();
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+
     }
+    return 0;
 }
