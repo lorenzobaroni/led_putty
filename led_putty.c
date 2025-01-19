b3883682bbb03d27e6be4c74dab1ecabfed3dfda
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-01-19 02:01:57 -0300

    Finalização do main

--- led_putty.c
+++ led_putty.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdio.h>
 #include <string.h>
 
 // Definição dos pinos para os LEDs e Buzzer
@@ -30,7 +30,7 @@ void all_off() {
 // Função para gerar tons no buzzer
 void tone(uint buzzer, uint frequencia, uint duracao) {
     uint32_t periodo = 1000000 / frequencia; 
-    uint32_t meio_periodo = periodo / 2;
+    uint32_t meio_periodo = periodo / 2;    
     uint32_t ciclos = frequencia * duracao / 1000;
 
     for (uint32_t i = 0; i < ciclos; i++) {
@@ -44,13 +44,46 @@ void tone(uint buzzer, uint frequencia, uint duracao) {
 int main() {
     stdio_init_all();
     init_gpio();
+    all_off();
+
+    char input[20];
 
     while (true) {
         printf("Digite um comando: ");
         if (fgets(input, sizeof(input), stdin)) {
+            // Remove o caractere de nova linha, se presente
+            input[strcspn(input, "\n")] = 0;
 
+            if (strcmp(input, "vermelho") == 0) {
+                all_off();
+                gpio_put(LED_RED, 1);
+                printf("ON: RED\n");
+            } else if (strcmp(input, "azul") == 0) {
+                all_off();
+                gpio_put(LED_BLUE, 1);
+                printf("ON: BLUE\n");
+            } else if (strcmp(input, "verde") == 0) {
+                all_off();
+                gpio_put(LED_GREEN, 1);
+                printf("ON: GREEN\n");
+            } else if (strcmp(input, "branco") == 0) {
+                all_off();
+                gpio_put(LED_RED, 1);
+                gpio_put(LED_GREEN, 1);
+                gpio_put(LED_BLUE, 1);
+                printf("ON: WHITE\n");
+            } else if (strcmp(input, "buzzer") == 0) {
+                all_off();
+                printf("ON: BUZZER\n");
+                tone(BUZZER, 200, 500);
+            } else if (strcmp(input, "OFF") == 0) {
+                all_off();
+                printf("SYSTEM: OFF\n");
+            } else {
+                printf("Comando desconhecido: %s\n", input);
+            }
+        }
     }
+
     return 0;
-    }
 }
-
